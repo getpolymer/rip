b9df078747aa2eb6903013c8cd5872d1f26df5dd
Nabarun Pal <pal.nabarun95@gmail.com>
2019-12-14 20:41:41 +0530

    (feat) server: Adds initial implementation of ReverseProxy

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/getpolymer/rip/server"
 
 func main() {
-	fmt.Println("vim-go")
+	server.StartReverseProxy()
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,56 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
+	url, _ := url.Parse(target)
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	proxy.ServeHTTP(res, req)
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	proxyMapping := map[string]string{
+		"bin-1.nbrn.xyz": "http://localhost:9001",
+		"bin-2.nbrn.xyz": "http://localhost:9002",
+		"bin-3.nbrn.xyz": "http://localhost:9003",
+	}
+
+	fmt.Printf("%+v \n", req.Header)
+
+	host := req.Header.Get("Host")
+	log.Println(host)
+	if host == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	backend, ok := proxyMapping[host]
+	if !ok {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	serveReverseProxy(backend, res, req)
+}
+
+// StartReverseProxy starts the reverse proxy
+func StartReverseProxy() {
+	log.Println("Starting Reverse Proxy on port 8080...")
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
